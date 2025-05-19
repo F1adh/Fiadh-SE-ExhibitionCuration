a44b8cb3a5aa5d504bd2aa4e29ebcb7ce781937d
Fiadh <fiadhfinnegan@hotmail.com>
2025-05-19 15:09:37 +0100

    Exhibition page and toolbar created

--- src/routes/Exhibitions.tsx
+++ src/routes/Exhibitions.tsx
@@ -8,10 +8,9 @@ function RouteComponent() {
   return (
     <main className="flex flex-col w-screen min-h-screen ">
       <menu className="w-screen min-h-20 bg-MintGreen border-black border-solid border-2">
-      <h2>Filter</h2>
-      
+        <h2>Filter</h2>
       </menu>
-      
+
       
     </main>
   )
