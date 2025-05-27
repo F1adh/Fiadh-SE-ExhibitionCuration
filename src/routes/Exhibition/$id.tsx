e0f9b6a8e799f693165fb800789552d75482aca5
Fiadh <fiadhfinnegan@hotmail.com>
2025-05-27 17:38:03 +0100

    Create page for individual item details

--- src/routes/Exhibition/$id.tsx
+++ src/routes/Exhibition/$id.tsx
@@ -1,3 +1,4 @@
+import harvardObjectApi from '@/api/harvardObjectApi'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -7,7 +8,23 @@ export const Route = createFileRoute('/Exhibition/$id')({
 
 function RouteComponent() {
   const { id } = Route.useParams()
-  
 
-  return <div>Hello "${id}"!</div>
+  const { data } = useQuery({
+    queryKey: ['harvardObjectRecord', id],
+    queryFn: () => harvardObjectApi(id),
+  })
+
+  return (
+    <section className='bg-Turquoise'>
+      {data && (
+        <article>
+          <img src={data.primaryimageurl || '/imageplaceholder.png'}></img>
+          <h4>{data.title}</h4>
+          <h4>{data.people?.[0]?.name}</h4>
+
+          
+        </article>
+      )}
+    </section>
+  )
 }
